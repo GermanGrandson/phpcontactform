05a4de143984006b38ba4dc182584f1406c6da46
German Grandson <ganieto1@gmail.com>
2017-04-05 13:02:35 -0400

    add gmail and smtp settings

--- contactform.php
+++ contactform.php
@@ -19,6 +19,34 @@ if(isset($_POST['name'], $_POST['email'], $_POST['message'])){
     }
   }
 
+  if(empty($errors)){
+    $mail = new PHPMailer;
+
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Username = 'ganieto1@gmail.com';
+    //$mail->Password = 
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port= 465;
+
+    $mail->isHTML();
+    $mail->Subject = 'Contact form submitted';
+    $mail->Body = 'From:' . $fields['name'] . '(' . $fields['email'] . ')<p>' . $fields['message'] . '</p>';
+
+    $mail->FromName = 'Contact';
+    $mail->AddAddress('ganieto1@gmail.com', 'German');
+    //$mail->AddReplyTo($fields['email'], $fields['name']);
+
+    if($mail->send()){
+      header('Location: thanks.php');
+      die();
+    } else{
+      $errors[] = 'Sorry something went wrong, please try agian.';
+    }
+
+  }
 
 } else {
   $errors[] = 'Somethign went wrong';
